11cc0a44511a895f5a5b507b3eb01ee0f930ea53
unknown <ricardo.costa_loureiro@hotmail.com>
2020-05-04 12:53:06 +0100

    Solved all iterations

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 class SortedList {
   constructor() {
     // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
     // ...
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   load(pos) {
     // ...
+    if (!this.items[pos]) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
   }
 
   max() {
     // ...
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
   }
 
   min() {
     // ...
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
   }
 
   sum() {
     // ...
+    return this.items.reduce((acc, value) => {
+      return acc + value;
+    }, 0);
   }
 
   avg() {
     // ...
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
   }
 }
 
